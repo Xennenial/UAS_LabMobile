241013ec33854b9a1f529122adb8174d4273d0d7
Exen <143600070+Xennenial@users.noreply.github.com>
2023-12-27 22:20:28 +0700

    hapus balek

--- app/src/main/java/com/mobile/mp3_final/api/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.mobile.mp3_final.api
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("api/digimon")
-    fun getAgents(): Call<List<ResponseItem>>
-}
